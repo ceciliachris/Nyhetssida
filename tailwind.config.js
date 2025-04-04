4882d52ceb3b55811e7ca28b6a91d1ad8e4d3948
ceciliachris <cecilia.christianssons@gmail.com>
2025-04-04 13:39:26 +0200

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
